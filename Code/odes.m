8ac6efc9354b7f1fdbb9c8cc60a190df6c866eca
chunruixu <44587745+chunruixu@users.noreply.github.com>
2018-10-31 20:29:19 -0400

    Update Code/odes.m

--- Code/odes.m
+++ Code/odes.m
@@ -13,15 +13,23 @@ for v1=2:99
 end
 dydt(100)=p.k1_pos*p.clpxp*y(200)-p.k1_neg*y(100);
 
-%%CpdR
+%%CpdR_f
 for v2=101:200
   dydt(v2)=p.ks_cpdr*y(v2+100)/(y(v2+100)+p.J1)+p.k2_pos*y(v2+100)-p.kd*y(v2-100)*y(v2)/(y(v2-100)+p.J2)-p.k2_neg*y(v2+100)*y(v2)/(y(v2+100)+p.J3);
 end
 
-%%CpdR~P
+%%CpdR_b
 for v3=201:300
-  dydt(v3)=p.k2_neg*y(v3)*y(v3-100)/(y(v3)+p.J3)-p,k2_pos*y(v3);
-end
+  dydt(v3)=
+
+
+%%CpdR~P_f
+
+
+
+
+%%CpdR~P_b
+
 
 %%%%%%%%%%%%%%%%%%%%%%
 dydt(301)=p.growth*y(301);
