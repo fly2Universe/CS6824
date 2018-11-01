e0d56b51e2f0d8ed25c46baa20da34a2fedc3d1d
Jacky <31670359+yao677@users.noreply.github.com>
2018-10-31 20:59:42 -0400

    modify odes.m

--- Code/tmp.m
+++ Code/tmp.m
@@ -1,18 +1,3 @@
-%odes.m
-
-
-function dydt=odes(t,y);
-global p;
-
-dydt=zeros(301,1);
-
-%%Complex1 (ClpXP:CpdR)
-dydt(1)=p.k1_pos*p.clpxp*y(101)-p.k1_neg*y(1);
-for v1=2:99
-  dydt(v1)=p.k1_pos*p.clpxp*y(v1+100)-p.k1_neg*y(v1);
-end
-dydt(100)=p.k1_pos*p.clpxp*y(200)-p.k1_neg*y(100);
-
 %%CpdR_f 
 for v2=101:200
   dydt(v2)=p.ks_cpdr*p.cckap/(p.cckap+p.J1)-p.kd*y(v2-100)*y(v2)/(y(v2-100)+p.J2)...
@@ -24,15 +9,3 @@ for v3=201:300
   dydt(v3)=-p.kd*y(v3-200)*y(v3)/(y(v3-200)+p.J2)+p.kcpdr_f_b*y(v3-100)-p.kcpdr_b_f*y(v3)...
       +p.k2_pos*y(v3+200)-p.k2_neg*p.cckap*y(v3)/(p.cckap+p.J3)+p.k1_neg*y(v3-200)-p.k1_pos*y(v3)*p.clpxp;
 end
-
-
-%%CpdR~P_f
-
-
-
-
-%%CpdR~P_b
-
-
-%%%%%%%%%%%%%%%%%%%%%%
-dydt(301)=p.growth*y(301);
