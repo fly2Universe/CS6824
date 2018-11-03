de30b8aad0032d793aebef62d1369e97098f9e64
Jacky <31670359+yao677@users.noreply.github.com>
2018-11-03 12:14:52 -0400

    add get_init.m

--- Code/get_init.m
+++ Code/get_init.m
@@ -2,3 +2,12 @@ function initial_cond = get_init(type)
 %% INITIAL CONDITIONS
 y0=zeros(501,1);
 y0(501) = 0.013;           % cell size
+
+%%pass parameter
+param(0, eval(type));
+%% INTEGRATION PARAMETERS
+t0 = 0;
+tf = 150;%150;        
+[t,y]=ode15s(@odes,[t0 tf],y0);
+
+initial_cond.init = y(end,:);
