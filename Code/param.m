e165453d25c99e8aac7fd632e20d6e5ee445b843
chunruixu <44587745+chunruixu@users.noreply.github.com>
2018-11-06 11:43:11 -0500

    Update Code/param.m

--- Code/param.m
+++ Code/param.m
@@ -16,5 +16,23 @@ T=150;%period of Caulobacter
 p.cckap=p1*t_d.^3 + p2*t_d.^2 + p3*t_d + p4;
 
 %% Synthesis and degradation rate constants [units --> 1/min]
-p.ks_cpdr=
-p.kd_cpdr=
+p.k1_pos=0.5 %forward reaction: ClpXP+CpdR ->Complex1
+p.k1_neg=0.2 %backward reaction: Complex1->ClpXP+CpdR
+p.ks_cpdr=0.02;
+p.kd_cpdr=0.01;
+%%phosporylation
+p.k2_pos=0.5;
+p.k2_neg=0.8;
+%% Diffusion parameters [units --> um^2/min]
+p.D_complex1=10;
+p.D_cpdr=100;
+p.D_cpdrp=100;
+%%
+p.J1=0.5;
+p.J2=0.5;
+p.J3=0.5;
+%%binding
+p.kcpdr_b_f=0.1;
+p.kcpdr_f_b=1;
+p.kcpdr_b_f=0.1;
+p.kcpdr_f_b=1;
