a31eb43fafbfaa92a9038ccae8dbce9b8d25b425
chunruixu <44587745+chunruixu@users.noreply.github.com>
2018-11-07 17:09:35 -0500

    Update Code/param.m

--- Code/param.m
+++ Code/param.m
@@ -6,14 +6,7 @@ else p.growth = 0.0055;
 end
 %%extra constant or function
 p.clpxp=1%um
-%%parameters of cckap's function
-p1 =  -3.491e-06;
-p2 =   0.0007817;
-p3 =    -0.04119 ;
-p4 =      0.7378;
-T=150;%period of Caulobacter
-t_d=rem(t,T); %return remainder after division t/T
-p.cckap=p1*t_d.^3 + p2*t_d.^2 + p3*t_d + p4;
+
 
 %% Synthesis and degradation rate constants [units --> 1/min]
 p.k1_pos=0.5 %forward reaction: ClpXP+CpdR ->Complex1
@@ -34,5 +27,5 @@ p.J3=0.5;
 %%binding
 p.kcpdr_b_f=0.1;
 p.kcpdr_f_b=1;
-p.kcpdr_b_f=0.1;
-p.kcpdr_f_b=1;
+p.kcpdrp_b_f=0.1;
+p.kcpdrp_f_b=1;
