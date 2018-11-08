203bf7f914354ebb46e7394a57b397158419459c
chunruixu <44587745+chunruixu@users.noreply.github.com>
2018-11-08 09:53:26 -0500

    Update Code/get_init.m

--- Code/get_init.m
+++ Code/get_init.m
@@ -1,14 +1,14 @@
 function initial_cond = get_init()
 %% INITIAL CONDITIONS
 y0=zeros(601,1);
-y0(501) = 0.013;           % cell size
+y0(601) = 0.013;           % cell size
 
 %%pass parameter
 param(0);
 %% INTEGRATION PARAMETERS
 t0 = 0;
 tf = 200;%150;        
-[t,y]=ode23(@odes,[t0 tf],y0);
+[t,y]=ode15s(@odes,[t0 tf],y0);
 %plot (t,y(:,1))
 %plot(t,y(:,501))
 initial_cond.init = y(end,:);
