bed34eef204fff8793bdfcdcc13f68e40cfcaa5f
chunruixu <44587745+chunruixu@users.noreply.github.com>
2018-11-07 20:49:51 -0500

    Update Code/get_init.m

--- Code/get_init.m
+++ Code/get_init.m
@@ -7,8 +7,8 @@ y0(501) = 0.013;           % cell size
 param(0);
 %% INTEGRATION PARAMETERS
 t0 = 0;
-tf = 2000;%150;        
-[t,y]=ode15s(@odes,[t0 tf],y0);
+tf = 200;%150;        
+[t,y]=ode45(@odes,[t0 tf],y0);
 plot (t,y(:,1))
 %plot(t,y(:,501))
 initial_cond.init = y(end,:);
