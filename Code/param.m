bb2d49704ca213bfeed37e2385ae7a22f86dde1a
chunruixu <44587745+chunruixu@users.noreply.github.com>
2018-11-10 21:07:04 -0500

    Update Code/param.m

--- Code/param.m
+++ Code/param.m
@@ -5,11 +5,11 @@ if cond == 0
 else p.growth = 0.0055;
 end
 %%extra constant or function
-p.clpxp=1;%um
+p.clpxp=0.5;%um
 
 
 %% Synthesis and degradation rate constants [units --> 1/min]
-p.k1_pos=0.5; %forward reaction: ClpXP+CpdR ->Complex1
+p.k1_pos=1; %forward reaction: ClpXP+CpdR ->Complex1
 p.k1_neg=0.2; %backward reaction: Complex1->ClpXP+CpdR
 p.ks_cpdr=2;%0.02;
 p.kd_cpdr=1;%0.01;
@@ -21,9 +21,9 @@ p.D_complex1=10;
 p.D_cpdr=100;
 p.D_cpdrp=100;
 %%
-p.J1=0.5;
+p.J1=10;
 p.J2=0.5;
-p.J3=0.1%%0.5;
+p.J3=0.5;%%0.5;
 %%binding
 p.kcpdr_b_f=0.1;
 p.kcpdr_f_b=1;
