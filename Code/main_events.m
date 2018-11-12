197dac184d4e23ac94ae41d8e2804411a65967a7
chunruixu <44587745+chunruixu@users.noreply.github.com>
2018-11-12 18:34:43 -0500

    Update Code/main_events.m

--- Code/main_events.m
+++ Code/main_events.m
@@ -58,7 +58,7 @@ end
 % Define Grid M for plot
 % The plot is basically a heat map of the concentration of the species along the main axis of the cell
 for n=51:100
-    M(:,n)=yout(:,501)*(n/100)-0.5*(yout(:,501)*(.5) + yout(:,501)*.51); 
+    M(:,n)=yout(:,701)*(n/100)-0.5*(yout(:,701)*(.5) + yout(:,701)*.51); 
     % Each element from column 51 to 100 is assigned the 'n'th fraction of 
     %total cell length at a given time step followed by subtracting the mid 
     %point value so that the centre of the grid takes the value 0
@@ -75,47 +75,71 @@ complex1(:,1:100)=yout(:,1:100);
 CpdR(:,1:100)=yout(:,101:200)+yout(:,201:300);
 %CpdR~p
 CpdR_p(:,1:100)=yout(:,301:400);
-
+%complex2
+complex2(:,1:100)=yout(:,501:600);
+%RcdA
+RcdA(:,1:100)=yout(:,601:700);
 %flip array ?
 complex1 = fliplr(complex1);
 CpdR = fliplr(CpdR);
 CpdR_p = fliplr(CpdR_p);
-
+complex2 = fliplr(complex2);
+RcdA = fliplr(RcdA);
 %transpose ?
 complex1 = complex1.';
 CpdR = CpdR.';
 CpdR_p = CpdR_p.';
+complex2 = complex2.';
+RcdA=RcdA.';
 M = M.';
 
 
-figure(1)
-ax1 = subplot(2,2,1);
-pcolor(tout, M, complex1)
-shading interp
-colorbar
-title('ClpXP:CpdR')
-xlim([0 120])
-xlabel('time (min)')
-label_str = strcat('cell size (',char(956),'m)');
-ylabel(label_str) 
+% figure(1)
+% ax1 = subplot(2,2,1);
+% pcolor(tout, M, complex1)
+% shading interp
+% colorbar
+% title('ClpXP:CpdR')
+% xlim([0 120])
+% xlabel('time (min)')
+% label_str = strcat('cell size (',char(956),'m)');
+% ylabel(label_str) 
+% 
+% ax2 = subplot(2,2,2);
+% pcolor(tout, M, CpdR)
+% shading interp
+% colorbar
+% xlim([0 120])
+% xlabel('time (min)')
+% label_str = strcat('cell size (',char(956),'m)');
+% ylabel(label_str) 
+% title('CpdR')
+% 
+% ax3 = subplot(2,2,3);
+% pcolor(tout, M, CpdR_p)
+% shading interp
+% colorbar
+% xlim([0 120])
+% xlabel('time (min)')
+% title('CpdR~p')
 
-ax2 = subplot(2,2,2);
-pcolor(tout, M, CpdR)
+figure(2)
+ax4 = subplot(2,2,1);
+pcolor(tout, M, complex2)
 shading interp
 colorbar
 xlim([0 120])
 xlabel('time (min)')
-label_str = strcat('cell size (',char(956),'m)');
-ylabel(label_str) 
-title('CpdR')
+title('complex2')
 
-ax3 = subplot(2,2,3);
-pcolor(tout, M, CpdR_p)
+ax5 = subplot(2,2,2);
+pcolor(tout, M, RcdA)
 shading interp
 colorbar
 xlim([0 120])
 xlabel('time (min)')
-title('CpdR~p')
+title('RcdA')
+
 label_str = strcat('cell size (',char(956),'m)');
 ylabel(label_str) 
 
