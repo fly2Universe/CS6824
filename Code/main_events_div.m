a3a23280ef731d6976e09e0c53fe5b445efc14b5
Jacky <31670359+yao677@users.noreply.github.com>
2018-11-13 23:22:49 -0500

    before moerge

--- Code/main_events_div.m
+++ Code/main_events_div.m
@@ -4,61 +4,24 @@ function main_events_div(sw_to_early_pd)
 param(1); %cell can grow
 
 %% INITIAL CONDITIONS
-y0 = sw_to_early_pd;
+y0 = sw_to_early_pd.endpoints;
 y0 = y0.';
 %% INTEGRATION PARAMETERS
 t0 = 0;
-tf = 120;
+tf = 30;
 
-options = odeset('Events',@event,'RelTol',1e-4,'AbsTol',1e-6);
-tout=t0;
-yout=y0.';
-teout = [];
-yeout = [];
-ieout = [];
+options = odeset('RelTol',1e-4,'AbsTol',1e-4);
 
 
-%simulate the system in a segmented fashion, which includes a series of
-%event
-while t0<tf
-    %solve system, integration is stopped when a specific event happens
-    [t,y,te,ye,ie]=ode15s(@odes_div,[t0 tf],y0,options);
-    %number of points calculated
-    nt=length(t);
-    %the time points and correspoding values calculated before the event
-    tout=[tout;t(2:nt)];
-    yout=[yout;y(2:nt,:)];
-    %record the time when an event happens, the correspoding y value and the event index 
-    teout = [teout;te];
-    yeout = [yeout;ye];
-    ieout = [ieout;ie];
-    
-    %reset the inition conditon
-    y0 = y(nt,:);
-    
-    %if something wrong
-    if isscalar(ie) == 0
-        ie = 0;
-    end
-    
-   
-    %enforced localization Sticky CpdR
-    if (ie==1)||(ie==2)
-        y0(501:511)=1;
-        y0(512:600)=0;
-    elseif ie==3
-        y0(501:600)=0;
-    end    
-    
-    t0=t(nt);
+%solve system, integration is stopped when a specific event happens
+[t,yout]=ode15s(@odes_div,[t0 tf],y0,options);
 
-end
 
 
 % Define Grid M for plot
 % The plot is basically a heat map of the concentration of the species along the main axis of the cell
 for n=51:100
-    M(:,n)=yout(:,601)*(n/100)-0.5*(yout(:,601)*(.5) + yout(:,601)*.51); 
+    M(:,n)=yout(:,501)*(n/100)-0.5*(yout(:,501)*(.5) + yout(:,501)*.51); 
     % Each element from column 51 to 100 is assigned the 'n'th fraction of 
     %total cell length at a given time step followed by subtracting the mid 
     %point value so that the centre of the grid takes the value 0
@@ -90,39 +53,34 @@ M = M.';
 
 figure(1)
 ax1 = subplot(2,2,1);
-pcolor(tout, M, complex1)
+pcolor(t, M, complex1)
 shading interp
 colorbar
 title('ClpXP:CpdR')
-xlim([0 120])
+xlim([0 30])
 xlabel('time (min)')
 label_str = strcat('cell size (',char(956),'m)');
 ylabel(label_str) 
 
 ax2 = subplot(2,2,2);
-pcolor(tout, M, CpdR)
+pcolor(t, M, CpdR)
 shading interp
 colorbar
-xlim([0 120])
+xlim([0 30])
 xlabel('time (min)')
 label_str = strcat('cell size (',char(956),'m)');
 ylabel(label_str) 
 title('CpdR')
 
 ax3 = subplot(2,2,3);
-pcolor(tout, M, CpdR_p)
+pcolor(t, M, CpdR_p)
 shading interp
 colorbar
-xlim([0 120])
+xlim([0 30])
 xlabel('time (min)')
 title('CpdR~p')
 label_str = strcat('cell size (',char(956),'m)');
 ylabel(label_str) 
-
-
-
-
-
 
 
 end
