d2fbbe89a23b76c81d497bba7886988be5604cd6
chunruixu <44587745+chunruixu@users.noreply.github.com>
2018-11-14 15:57:11 -0500

    Update Code/param.m

--- Code/param.m
+++ Code/param.m
@@ -10,12 +10,12 @@ p.clpxp=0.5;%um
 
 %% Synthesis and degradation rate constants [units --> 1/min]
 p.k1_pos=2; %forward reaction: ClpXP+CpdR ->Complex1
-p.k1_neg=0.2; %backward reaction: Complex1->ClpXP+CpdR
+p.k1_neg=15; %backward reaction: Complex1->ClpXP+CpdR
 p.ks_cpdr=2;%0.02;
 p.kd_cpdr=1;%0.01;
 %%phosporylation
 p.k2_pos=2;%0.5;
-p.k2_neg=8;%0.8;%based on ratio of p/unp
+p.k2_neg=15;%0.8;%based on ratio of p/unp
 %%
 p.k3_pos=20;%forward reaction: complex1+RcdA ->Complex1
 p.k3_neg=0.2;%backward reaction: complex1+RcdA
@@ -32,11 +32,11 @@ p.J1=10;
 p.J2=0.5;
 p.J3=0.5;%%0.5;
 p.J4=0.4;
-p.km1=3;
+p.km1=30;
 p.J5=0.5;
 p.J6=2;
 %%binding
 p.kcpdr_b_f=0.1;
-p.kcpdr_f_b=5;
+p.kcpdr_f_b=15;
 p.kcpdrp_b_f=0.5;
 p.kcpdrp_f_b=1;
